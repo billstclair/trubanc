a8139c76b94be7ba520a9a9714077a9112cad27e
Bill St. Clair <billstclair@gmail.com>
2008-08-20 07:24:21 -0400

    index.php edits

--- index.php
+++ index.php
@@ -39,6 +39,7 @@ if ($msg) {
 <p>
 <img src="trubanc-logo.jpg" alt="Truebanc" width="376" height="355"/>
 <p>
+<table width="75%"><tr><td>
 <a href="http://Trubanc.com/">Trubanc</a> is an anonymous,
 digitally-signed vault and trading system. Like
 <a href="https://loom.cc/">Loom</a>, it allows anyone to issue assets
@@ -50,8 +51,13 @@ destruction of transaction history for closed trades. Trubanc will
 initially provide server-based trading. Eventually, it will provide
 digital checks and bearer certificates. These, however, WILL require
 permanent storage of transaction history.
+</td></tr></table>
 <p>
 <a href="plain-english.html">Trubanc in Plain English</a>
+<p>
+<table width="75%"><tr><td>
+This page is a live server. Invoke it as "http://trubanc.com/?msg=&lt;msg>", and it will interpret &lt;msg> as a server request, and return the result. For example, <a href="http://trubanc.com/?debug=true&msg=(bc50c4fd9c228a21f64d34ca644a46c1fe8520e4%2Cbankid%2C-----BEGIN+PUBLIC+KEY-----%0AMFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAMwfcmkk2coTuYAEbdZ5iXggObNPzbSi%0ADnVtndZFe4%2F4Xg0IQPfpQ04OkhWIftMy1OjFhGlBzzNzdW98KYwKMgsCAwEAAQ%3D%3D%0A-----END+PUBLIC+KEY-----%0A)%3A%0AsLJ9GqFjZ61fq%2FbDFL6rxpY3w2s5dWIAXJCvPKQTPEkrG%2F2I1fwxBfugBmn%2FiPwa%0AjCRtnFDnrn7Mv%2BUY%2BSH4yw%3D%3D">click here</a> to send a "bankid" request, with debugging enabled to make it easy to see. I'm working on a web client, which I'll link to here when it's ready to test.
+</td></tr></table>
 <p>
 You may view unfinished code in progress <a href="viewtext.php">here</a>.
 </center>
